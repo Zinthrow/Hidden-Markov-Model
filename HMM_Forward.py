fe1119c200fcb03b71390ce551eb2fa126fda381
Zinthrow <33006657+Zinthrow@users.noreply.github.com>
2017-11-22 13:54:33 -0600

    Update HMM_Forward.py

--- HMM_Forward.py
+++ HMM_Forward.py
@@ -6,32 +6,32 @@ Created on Fri Nov 17 10:46:39 2017
 
 import os
 
-class Node():
-    def __init__(self):
-        self.score = None
-        self.child = []
-        self.parent = []
- 
 # Find the transitions file, open and extract values    
 filename = "transition2.txt"
 if os.path.exists(filename):
     lines = open(filename, 'r') 
     lines = lines.readlines()
 a = {} #transition state probabilities lookup dictionary
+l = -1 #number of states
 for x in lines:
     x = x.split()
-    a[x[0]+x[1]] = float(x[2])
- 
+    if int(x[1]) > l:
+        l = int(x[1])
+    if x[1] in a:
+        a[x[1]].append([x[0],float(x[2])]) 
+    else:
+        a[x[1]] = []
+        a[x[1]].append([x[0],float(x[2])])
+        
 # Find the emissions file, open and extract values    
 filename = "emission2.txt"
 if os.path.exists(filename):
     lines = open(filename, 'r') 
     lines = lines.readlines()
 e = {} #emission probabilities for each position
-e_nuc = ['A','C','G','T'] # the corresponding nucleotides for the emission probabilities
+e_nuc = ['A','C','G','T'] #nucleotides for the emission probabilities
 for x in lines:
     x= x.split()
-    e_nuc.append(x[1])
     if x[0] in e:
         e[x[0]].append(float(x[2]))
     else:
@@ -39,12 +39,25 @@ for x in lines:
         e[x[0]].append(float(x[2]))
         
 seq = "TAGA"
-f = []
 
-print (a)
-'''
+class Forward():
+    def __init__(self, seq):
+        self.f = ["a"]
+        self.e = e
+        self.a = a
+        self.e_nuc = e_nuc
+        self.l = l
+        self.seq = seq
+    def get_f(self):
+        return self.f
+    def implement(self):
+        print (self.seq)
 for y in range(len(seq)+1):
-    for x in
-    
-'''
+    for x in range(l):
+        if y == 0:
+            continue
+
+forward = Forward(seq)
+forward.implement()
+
 
