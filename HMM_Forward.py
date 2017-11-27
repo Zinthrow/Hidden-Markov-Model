a9170b964cdf3c2edf959727a42579370eb3eac5
Zinthrow <33006657+Zinthrow@users.noreply.github.com>
2017-11-27 13:13:39 -0600

    Update HMM_Forward.py

--- HMM_Forward.py
+++ HMM_Forward.py
@@ -7,20 +7,20 @@ import os
 
 # Find the transitions file, open and extract values    
 filename = "transition2.txt"
-if os.path.exists(filename):
+if os.path.exists(filename): 
     lines = open(filename, 'r') 
     lines = lines.readlines()
 a = {} #transition state probabilities lookup dictionary
 l = -1 #number of states
-for x in lines:
+for x in lines: 
     x = x.split()
     if int(x[1]) > l:
         l = int(x[1])
     if int(x[1]) in a:
-        a[int(x[1])].append([int(x[0]), float(x[2])]) 
+        a[int(x[1])].append([int(x[0]), float(x[2])]) # after the key exists this appends the key's extra transition states
     else:
-        a[int(x[1])] = []
-        a[int(x[1])].append([int(x[0]), float(x[2])])
+        a[int(x[1])] = []#transition states are saved in a dict format that the key is what is being transitioned to
+        a[int(x[1])].append([int(x[0]), float(x[2])]) #inside that key is a list of the original state and the probability to transition to the key
         
 # Find the emissions file, open and extract values    
 filename = "emission2.txt"
@@ -34,34 +34,34 @@ for x in lines:
     if int(x[0]) in e:
         e[int(x[0])].append(float(x[2]))
     else:
-        e[int(x[0])] = []
+        e[int(x[0])] = []#Makes a dictionary with a key that signifies the current state "l" and the corresponding nucleotide probabilities
         e[int(x[0])].append(float(x[2]))
         
 seq = "TAGA"
 
 class Forward():
     def __init__(self, seq):
-        self.f = []
-        self.e = e
-        self.a = a
-        self.e_nuc = e_nuc
-        self.l = l
-        self.seq = seq
-    def get_f(self):
+        self.f = [] # table that holds all probabilities
+        self.e = e # emission states
+        self.a = a # transmission states
+        self.e_nuc = e_nuc # nucleotide order in emmissions dictionary
+        self.l = l # number of states
+        self.seq = seq # the sequence being compared
+    def get_f(self): # print results
         print (self.f)
         print ("Last state sum:" +str(self.get_sum(len(self.seq),l-1)))
         
-    def get_sum(self,i,j):
+    def get_sum(self,i,j): # find the sum of all states that leads to the probability at f given state in the previous nucleotides 
         f = self.f
         a = self.a
         path_sum = 0
         for x in a[i+1]:
-            if j == 0 and x[0] == 0:
+            if j == 0 and x[0] == 0: #for the paths that lead from the original state
                 path_sum += x[1]
-            elif j != 0 and x[0] != 0:
+            elif j != 0 and x[0] != 0:# for all other paths
                 path_sum += (f[j-1][x[0]-1]*x[1])
         return path_sum 
-    def get_emission(self,i,j):
+    def get_emission(self,i,j):# returns the emission of every nucleotide given state "l"
         e = self.e
         seq = self.seq
         if seq[j] == 'A':
@@ -72,7 +72,7 @@ class Forward():
             return e[i+1][2]
         elif seq[j] == 'T':
             return e[i+1][3]
-    def implement(self):
+    def implement(self):#multiplies the Sum of previous applicable values in "f" by the emission given state "l"
         l = self.l
         seq = self.seq
         for y in range(len(seq)):
@@ -85,7 +85,7 @@ class Forward():
                     
 
 forward = Forward(seq)
-print (a)
-print (e)
+print (a) 
+print (e) 
 forward.implement()
 forward.get_f()
